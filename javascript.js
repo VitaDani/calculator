9a4e14e3b55f988a1d5ed4b8f895a68bf8ee8370
Daniel Vitacolonna <44033488+VitaDani@users.noreply.github.com>
2024-08-20 14:17:29 -0600

    Create the index, style, and javascript files.
    Begin the index, style, and javascript files and begin implementing the calculator functionality.

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/* Create the functions for the mathematical operations:
+ Add, Subtract, Multiply and Divide */
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
