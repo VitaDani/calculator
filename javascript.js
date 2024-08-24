afaee4f8ee8c9e39fe5f1a8efd2893b9e4936ce2
Daniel Vitacolonna <44033488+VitaDani@users.noreply.github.com>
2024-08-24 12:02:29 -0600

    Create the variables to do math and display it.
    Creating the variables for the inputs, and writing the logic to perform the math operations.

--- javascript.js
+++ javascript.js
@@ -15,3 +15,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return (a / b);
 }
+
+/* Create the variables that will be used for mathematical operations and display*/
+let firstNumber;
+let secondNumber;
+let operation;
+
+/* Create the function that takes the variables and performs the operation */
+function operate(operation, firstNumber, secondNumber) {
+    if(operation === divideButton) {
+        return divide(firstNumber, secondNumber);
+    } else if(operation === multiplyButton) {
+        return multiply(firstNumber, secondNumber);
+    } else if(operation === minusButton) {
+        return subtract(firstNumber, secondNumber);
+    } else if(operation === plusButton) {
+        return add(firstNumber, secondNumber);
+    }
+}
