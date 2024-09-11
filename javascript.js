9bfa4ac20495e1e71b5154af7cab872d110034df
Daniel Vitacolonna <44033488+VitaDani@users.noreply.github.com>
2024-09-11 12:57:28 -0600

    FINALLY figure out the logic to make it work.
    Add the functions to create the calculation functionality for the calculator.

--- javascript.js
+++ javascript.js
@@ -1,82 +1,75 @@
 /* Create the functions for the mathematical operations:
  Add, Subtract, Multiply and Divide */
- function add(a, b) {
+ function sum(a, b) {
     return (a + b);
 }
 
-function subtract(a, b) {
+function minus(a, b) {
     return (a - b);
 }
 
-function multiply(a, b) {
+function product(a, b) {
     return (a * b);
 }
 
-function divide(a, b) {
+function quotient(a, b) {
     return (a / b);
 }
 
 /* Create the variables that will be used for mathematical operations and display*/
 const display = document.querySelector("#display");
-let firstNumber;
-let secondNumber;
+display.textContent = '0';
+let firstNumber = 0;
+let secondNumber = 0;
 let operation;
 
 /* Create the function that takes the variables and performs the operation */
 function operate(operation, firstNumber, secondNumber) {
-    if(operation === divideButton) {
-        return divide(firstNumber, secondNumber);
-    } else if(operation === multiplyButton) {
-        return multiply(firstNumber, secondNumber);
-    } else if(operation === minusButton) {
-        return subtract(firstNumber, secondNumber);
-    } else if(operation === plusButton) {
-        return add(firstNumber, secondNumber);
+    if(operation === "/") {
+        return quotient(firstNumber, secondNumber);
+    } else if(operation === "*") {
+        return product(firstNumber, secondNumber);
+    } else if(operation === "-") {
+        return minus(firstNumber, secondNumber);
+    } else if(operation === "+") {
+        return sum(firstNumber, secondNumber);
     }
 }
 
 /* Create the functions to update the display once buttons are clicked */
 function updateDisplay(displayValue) {
+    if(display.textContent === '0') {
+        display.textContent = '';
+    }
     display.textContent += displayValue;
 }
 
 const buttons = document.querySelector("#buttons");
-buttons.addEventListener('click', (event) => {
+buttons.addEventListener('mousedown', (event) => {
     let target = event.target;
 
     switch(target.id) {
         case 'zero':
-            updateDisplay(0);
-            break;
         case 'one':
-            updateDisplay(1);
-            break;
         case 'two':
-            updateDisplay(2);
-            break;
         case 'three':
-            updateDisplay(3);
-            break;
         case 'four':
-            updateDisplay(4);
-            break;
         case 'five':
-            updateDisplay(5);
-            break;
         case 'six':
-            updateDisplay(6);
-            break;
         case 'seven':
-            updateDisplay(7);
-            break;
         case 'eight':
-            updateDisplay(8);
-            break;
         case 'nine':
-            updateDisplay(9);
+            updateDisplay(target.innerHTML);
             break;
         case 'add':
+        case 'subtract':
+        case 'mulitply':
+        case 'divide':
             operateButtonHit();
+            operation = target.innerHTML;
+            break;
+        case 'equals':
+            equalButtonHit();
             break;
     }
 });
@@ -90,4 +83,5 @@ function operateButtonHit() {
 
 function equalButtonHit() {
     secondNumber = +display.textContent;
+    display.textContent = operate(operation, firstNumber, secondNumber);
 }
